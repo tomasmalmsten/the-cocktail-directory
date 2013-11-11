ee46e07da7401d4283e07cab7fb8a155456ace70
Tomas Malmsten <code@tomasmalmsten.com>
2013-11-11 16:26:48 +0100

    Cleaning up to start with a new template

--- project.clj
+++
@@ -1,9 +0,0 @@
-(defproject the-cocktail-directory "0.1.0-SNAPSHOT"
-  :description "FIXME: write description"
-  :url "http://example.com/FIXME"
-  :license {:name "GNU General Public Licens"
-            :url "http://www.gnu.org/licenses/gpl.txt"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
-  :main ^:skip-aot the-cocktail-directory.core
-  :target-path "target/%s"
-  :profiles {:uberjar {:aot :all}})
